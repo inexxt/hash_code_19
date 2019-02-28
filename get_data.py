1060f21dfefa1a3a2f09c53e0bbc068b64141b64
Jacek Olczyk <jacek.olczyk98@gmail.com>
2019-02-28 21:33:24 +0100

    Add split hv.

--- get_data.py
+++ get_data.py
@@ -2,7 +2,10 @@ import numpy as np
 import pandas as pd
 
 
-def get_data(filename, keep_hv=False):
+def get_data(filename, keep_hv=False, split_hv=False):
+    if split_hv and not keep_hv:
+        print("Assuming keep_hv...")
+        keep_hv = True
     path = "data/" + filename
     with open(path, "r") as f:
         data = f.read()[:-1]
@@ -12,6 +15,8 @@ def get_data(filename, keep_hv=False):
     tags = pd.unique(df[df.columns[ignore:]].values.ravel('K'))
     tags = np.array([None] + [i for i in tags if i is not None])
     df.replace(tags, np.arange(tags.shape[0]), inplace=True)
+    if split_hv:
+        return {'H': df[df[0] == 'H'].to_numpy(), 'V': df[df[0] == 'V'].to_numpy()}
     return df.to_numpy()
 
 
@@ -23,4 +28,4 @@ filenames = {
     'e': 'e_shiny_selfies.txt'
 }
 if __name__ == '__main__':
-    get_data(filenames['a'])
+    print(get_data(filenames['e'], keep_hv=True, split_hv=True))
